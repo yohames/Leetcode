65e42ef241e4068cfc9c0d993f29bb369e5e97d1
yohames <jonimsfn@gmail.com>
2025-12-27 11:23:10 +0300

    challenge h_262

--- db/pg/h_262.sql
+++ db/pg/h_262.sql
@@ -79,25 +79,24 @@ Output:
 +------------+-------------------+
 Explanation: 
 On 2013-10-01:
-  - There were 4 requests in total, 2 of which were canceled.
-  - However, the request with Id=2 was made by a banned client (User_Id=2), so it is ignored in the calculation.
-  - Hence there are 3 unbanned requests in total, 1 of which was canceled.
-  - The Cancellation Rate is (1 / 3) = 0.33
+ - There were 4 requests in total, 2 of which were canceled.
+ - However, the request with Id=2 was made by a banned client (User_Id=2), so it is ignored in the calculation.
+ - Hence there are 3 unbanned requests in total, 1 of which was canceled.
+ - The Cancellation Rate is (1 / 3) = 0.33
 On 2013-10-02:
-  - There were 3 requests in total, 0 of which were canceled.
-  - The request with Id=6 was made by a banned client, so it is ignored.
-  - Hence there are 2 unbanned requests in total, 0 of which were canceled.
-  - The Cancellation Rate is (0 / 2) = 0.00
+ - There were 3 requests in total, 0 of which were canceled.
+ - The request with Id=6 was made by a banned client, so it is ignored.
+ - Hence there are 2 unbanned requests in total, 0 of which were canceled.
+ - The Cancellation Rate is (0 / 2) = 0.00
 On 2013-10-03:
-  - There were 3 requests in total, 1 of which was canceled.
-  - The request with Id=8 was made by a banned client, so it is ignored.
-  - Hence there are 2 unbanned request in total, 1 of which were canceled.
-  - The Cancellation Rate is (1 / 2) = 0.50
-*/
-
+ - There were 3 requests in total, 1 of which was canceled.
+ - The request with Id=8 was made by a banned client, so it is ignored.
+ - Hence there are 2 unbanned request in total, 1 of which were canceled.
+ - The Cancellation Rate is (1 / 2) = 0.50
+ */
 -- TestCase 1
 -- CREATE TYPE status AS ENUM ('completed', 'cancelled_by_driver', 'cancelled_by_client');
--- CREATE TYPE role AS ENUM ('client', 'dirver', 'partner')
+-- CREATE TYPE role AS ENUM ('client', 'driver', 'partner')
 -- Create table If Not Exists Trips (id int, client_id int, driver_id int, city_id int, status status, request_at varchar(50))
 -- Create table If Not Exists Users (users_id int, banned varchar(50), role role)
 -- Truncate table Trips
@@ -119,30 +118,36 @@ On 2013-10-03:
 -- ,('10', 'No', 'driver')
 -- ,('11', 'No', 'driver')
 -- ,('12', 'No', 'driver')
--- ,('13', 'No', 'driver')
-
+-- ,('13', 'No', 'driver');
 -- Updates enum value
 -- ALTER TYPE role RENAME VALUE 'dirver' TO 'driver';
+-- dividing the number of canceled (by client or driver) requests with unbanned users by the total number of requests with unbanned users on that day.
+-- cancellation_rate = unbanned_canceled_req/total_unbanned_req
 
 
-dividing the number of canceled (by client or driver) requests with unbanned users by the total number of requests with unbanned users on that day.
+WITH UnbannedTrips AS (
+  SELECT
+    id,
+    request_at AS Day,
+    CASE WHEN trips.status IN ('cancelled_by_client', 'cancelled_by_driver') THEN
+      1
+    ELSE
+      0
+    END AS Is_Canceled
+  FROM
+    trips
+    JOIN users AS client ON trips.client_id = client.users_id
+    JOIN users AS driver ON trips.driver_id = driver.users_id
+  WHERE
+    client.banned = 'No'
+    AND driver.banned = 'No'
+    AND trips.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+)
+SELECT
+  Day,
+  Round(SUM(is_canceled)::decimal / NULLIF(count(*), 0), 2) AS "Cancellation Rate"
+FROM
+  UnbannedTrips
+GROUP BY
+  Day;
 
-cancellation_rate = unbanned_canceled_req/total_unbanned_req
-
-
-
-select * from trips 
-left join users as client
-on trips.client_id = client.users_id
-left join users as driver
-on trips.driver_id = driver.users_id
-where driver.banned = 'No'
-AND client.banned = 'No'
-AND (status = 'cancelled_by_client' OR status = 'cancelled_by_driver')
-
-
-select * from trips
-left join users as client
-on trips.client_id = client.users_id
-left join users as driver
-on trips.driver_id = client.users_id
